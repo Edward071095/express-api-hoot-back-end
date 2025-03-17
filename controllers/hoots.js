d3f0114a1dec3a9a2c8183cfef61948bd941068b
EdwardW710 <EdwardW710@yahoo.com>
2025-03-17 13:56:03 -0400

    completed crud funtionality

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const verifyToken = require("../middleware/verify-token.js");
 const Hoot = require("../models/hoot.js");
+const { deleteModel } = require("mongoose");
 const router = express.Router();
 
 // add routes here
@@ -27,6 +28,7 @@ router.get("/", verifyToken, async (req, res) => {
     }
 });
 
+// GET - /hoots/:hootId
 router.get("/:hootId", verifyToken, async (req, res) => {
     try {
         const hoot = await Hoot.findById(req.params.hootId).populate("author");
@@ -35,5 +37,65 @@ router.get("/:hootId", verifyToken, async (req, res) => {
         res.status(500).json({ err: err.message });
       }    
 });
+
+// PUT - /hoots/:hootId
+router.put("/:hootId", verifyToken, async (req, res) => {
+    try {
+      const hoot = await Hoot.findById(req.params.hootId);
+      
+      if (!hoot.author.equals(req.user._id)) {
+        return res.status(403).send("You're not allowed to do that!");
+      }
+
+      const updateHoot= await Hoot.findByIdAndUpdate(
+        req.params.hootId,
+        req.body,
+        { new: true }
+      );
+
+      res.status(200).json(updateHoot)
+    } catch (err) {
+      res.status(500).json({ err: err.message });  
+    }
+});
+
+// DELETE /HOOTS/:hootId
+router.delete("/:hootId", verifyToken, async (req, res) => {
+    try {
+      const hoot = await Hoot.findById(req.params.hootId);
+
+      if (!hoot.author.equals(req.user._id)) {
+        return res.status(403).send("You're not allowed to do that!");
+      }
+
+      const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
+      res.status(200).json(deletedHoot);
+    } catch (err) {
+      res.status(500).json({ err: err.message });
+    }
+});
+
+//POST /hoots/:hootId/comments
+router.post("/:hootId/comments", verifyToken, async (req, res) => {
+    try {
+      req.body.author = req.user._id;
+      const hoot = await Hoot.findById(req.params.hootId).populate([
+        'author',
+        'comments.author',
+      ]);
+      hoot.comments.push(req.body);
+      await hoot.save();
+      
+      const newComment = hoot.comments[hoot.comments.length - 1];
+
+      newComment._doc.author = req.user;
+
+      res.status(201).json(newComment)
+    } catch (err) {
+      res.status(500).json({ err: err.message });  
+    }
+});
+  
+  
 
 module.exports = router;
